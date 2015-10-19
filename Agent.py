9e91af6b834241b8accd040f2a09d73f5ec609bc
haywire <prashant.k@tyroo.com>
2015-10-20 01:01:02 +0530

    introduced basic triangularization approach

--- Agent.py
+++ Agent.py
@@ -15,6 +15,7 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 
 class Agent:
@@ -59,26 +60,29 @@ class Agent:
     # Make sure to return your answer *as an integer* at the end of Solve().
     # Returning your answer as a string may cause your program to crash.
     def Solve(self,problem):
-       print "Evaluating "+problem.name
+        print "Evaluating "+problem.name
 
-       imageList = ['A','B','C']
-       imageMatrixArr = []
+        imageList = ['A','B','C']
+        imageMatrixArr = []
 
-       for img in imageList :
+        for img in imageList :
            image = Image.open(problem.figures[img].visualFilename)
            imageMatrixArr.append(self.imageToPixelMatrix(image))
-       (w, h) = image.size
+        (w, h) = image.size
 
-       #breaking the list in different individual list
-       A = imageMatrixArr[0]
-       B = imageMatrixArr[1]
-       C = imageMatrixArr[2]
-       D = np.zeros((w, h)) #[[0 for x in range(h)] for x in range(w)]
-       E = np.zeros((w, h)) #[[0 for x in range(h)] for x in range(w)]
+        #breaking the list in different individual list
+        A = imageMatrixArr[0]
+        B = imageMatrixArr[1]
+        C = imageMatrixArr[2]
+        D = np.zeros((w, h)) #[[0 for x in range(h)] for x in range(w)]
+        E = np.zeros((w, h)) #[[0 for x in range(h)] for x in range(w)]
+
+        trA = self.triangularize(A, w, h)
+        trB = self.triangularize(B, w, h)
 
 
-       # populate D based on the relation between A and B
-       for x in range(0, w) :
+        # populate D based on the relation between A and B
+        for x in range(0, w) :
            for y in range(0, h) :
                if A[x][y] == B[x][y] :
                    D[x][y] = C[x][y]
@@ -91,20 +95,30 @@ class Agent:
                    E[x][y] = 255 - B[x][y]
 
 
-       ansImg = Image.fromarray(D)
-       ansImg.show()
+        ansImg = Image.fromarray(D)
+        ansImg.show()
 
-       optionsImageList = ['1','2','3','4','5','6']
-       optionsImageMatrix = []
-       # print "D: "
-       # print D
 
-       i = 0
-       for img in optionsImageList:
+        optionsImageList = ['1','2','3','4','5','6']
+        optionsImageMatrix = []
+        # print "D: "
+        # print D
+
+        i = 0
+        for img in optionsImageList:
            image = Image.open(problem.figures[img].visualFilename)
            imageMatrix = self.imageToPixelMatrix(image)
            # print optionsImageList[i]+": "
            # print imageMatrix
+           #     0    1    2    3
+           #   ____________________________
+           #0  | 0    1    1    0    *    *
+           #1  | 1    1    0    0    *    *
+           #2  | 0    1    1    0    *    *
+           #3  | 1    0    0    1    *    *
+           #   | *    *    *    *    *    *
+           #   | *    *    *    *    *    *
+           #   | *    *    *    *    *    *
 
            if (imageMatrix == D).all():
                 print "Answer: " + problem.figures[img].name
@@ -118,9 +132,47 @@ class Agent:
 
            #optionsImageMatrix.append(self.imageToPixelMatrix(image))
 
-       print "No match found\n"
-       # r, g, b , a = im.split()
-       # variable = Image.merge("RGB", (b,g,r))
-       # print(np.asarray(variable))
-       # exit()
-       return -1
+        print "No match found\n"
+        # r, g, b , a = im.split()
+        # variable = Image.merge("RGB", (b,g,r))
+        # print(np.asarray(variable))
+        # exit()
+        return -1
+
+    def test(self):
+        A = np.zeros((4,4))
+        A[0][0] = A[0][1] = A[1][0] = A[1][1] = 1
+        #A[0][2] = 1
+        A[2][1] = A[3][0] = 1
+        A[2][2] = A[3][3] = 1
+        rA = self.triangularize(A, 4, 4)
+
+    def triangularize(self, A, w, h):
+        attrib = {}
+        rA = np.zeros((int(w/2),int(h/2)))
+        for x in range(0,int(w/2)):
+            for y in range(0,int(h/2)):
+                bx = x*2
+                by = y*2
+                print "\nx,y = "+str(x)+","+str(y)
+                print "bx,by = "+str(bx)+","+str(by)
+                #tmpA = np.zeros(2,2)
+                rA[x][y] = 0
+                for i in range(0,2):
+                    for j in range(0,2):
+                        tmp = 1 if (A[bx+i][by+j]==0) else -1
+                        placeVal = int(math.pow(3, (i*2 + j*1)))
+                        print "placeval:"+str(i)+","+str(j)+"="+str(placeVal)+"*"+str(tmp)
+                        rA[x][y] += placeVal * tmp
+                        print "rA["+str(x)+"]["+str(y)+"] = "+str(rA[x][y])
+                shapeId = rA[x][y]
+
+                if shapeId in attrib:
+                    attrib[str(shapeId)] += 1
+                else:
+                    attrib[str(shapeId)] = 1
+
+        return { 'shapeMatrix': rA, 'attribs': attrib }
+
+#agent = Agent()
+#agent.test()
